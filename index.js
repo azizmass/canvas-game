bda0896905489ab010addd734a3dee936dca4932
azizmass <93921997+azizmass@users.noreply.github.com>
2022-02-02 00:07:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ const angle =Math.atan2(canvas.height/2-y,canvas.width/2-x)
     },1000)
 }
 
- window.addEventListener("click",function(event){
+ window.addEventListener("touchstart",function(event){
     const angle =Math.atan2(event.clientY-canvas.height/2,event.clientX-canvas.width/2)
      projectiles.push(new Projectile(x,y,5,'white',{x:Math.cos(angle)*5,y:Math.sin(angle)*5}))
      
